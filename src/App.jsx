4487d8e15f72b2f43bc7cface1019d140ca5ba94
yuval miles <yuvalmiles@gmail.com>
2022-09-11 11:23:00 +0300

    auth hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { Outlet } from "react-router-dom";
 import { useAuth } from "./hooks/useAuth";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
 function App() {
-  const { isLoading } = useAuth();
+  const { isLoading, token } = useAuth();
+  useEffect(() => {
+    if (token) {
+      const socket = new io("http://localhost:3000", {
+        extraHeaders: {
+          Authentication: `Bearer ${token.raw}`,
+        },
+      });
+      socket.emit("message", "hello");
+    }
+  }, [token]);
   if (isLoading) return <div></div>;
   return (
     <>
--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,10 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import axiosClient from "../utils/axiosClient";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const useAuth = () => {
   const navigate = useNavigate();
-  const { isLoading } = useQuery(
+  const location = useLocation();
+  const [errorAlert, setErrorAlert] = useState({ show: false, message: "" });
+  const { isLoading: authLoading, data: token } = useQuery(
     ["auth"],
     async () => (await axiosClient.get("/auth")).data,
     {
@@ -12,8 +15,47 @@ export const useAuth = () => {
       retry: false,
       onSuccess: (data) => {
         if (!data) navigate("/");
+        else if (location.pathname === "/") navigate("/feed");
+      },
+      onError: (error) => {
+        if (error.response.data)
+          setErrorAlert({ show: true, message: error.response.data.message });
+        else
+          setErrorAlert({
+            show: true,
+            message: "Ooops an error has occurred please try again later",
+          });
       },
     }
   );
-  return { isLoading };
+  const { refetch: logout, isLoading: logoutLoading } = useQuery(
+    ["logout"],
+    async () => (await axiosClient.get("/auth/signout")).data,
+    {
+      cacheTime: 0,
+      enabled: false,
+      refetchOnWindowFocus: false,
+      onSuccess: () => {
+        navigate("/");
+      },
+      onError: (error) => {
+        if (error.response.data)
+          setErrorAlert({ show: true, message: error.response.data.message });
+        else
+          setErrorAlert({
+            show: true,
+            message: "Ooops an error has occurred please try again later",
+          });
+      },
+    }
+  );
+  const closeErrorAlert = () => setErrorAlert({ show: false, message: "" });
+  return {
+    token,
+    authLoading,
+    logoutLoading,
+    logout,
+    errorAlert,
+    closeErrorAlert,
+  };
 };
