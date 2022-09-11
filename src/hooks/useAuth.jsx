44c0140d86f1c67eddf961f0395c59b70cd23b0e
Myribi <105745901+Myribi@users.noreply.github.com>
2022-09-11 11:10:39 +0300

    feed violation error

---
+++ src/hooks/useAuth.jsx
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import axiosClient from "../utils/axiosClient";
+import { useNavigate } from "react-router-dom";
+
+export const useAuth = () => {
+  const navigate = useNavigate();
+  const { isLoading, data: userData } = useQuery(
+    ["auth"],
+    async () => (await axiosClient.get("/auth")).data,
+    {
+      cacheTime: 0,
+      retry: false,
+      onSuccess: (data) => {
+        if (!data) navigate("/");
+      },
+    }
+   
+  );
+  return { isLoading,userData };
+};
+
+
+
+
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,53 +1,45 @@
 import { Box } from "@mui/material";
-import "./Feed.css";
-import React from "react";
-import AddIcon from '@mui/icons-material/Add';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Button from "@mui/material/Button";
+import CreatePostForm from "../components/posts/CreatePostForm";
+import PostsList from "../components/posts/PostsList";
+import { useQuery } from "@tanstack/react-query";
+import axiosClient from "../utils/axiosClient";
 
 const Feed = () => {
-  return(
+  const [postsList, setPostsList] = useState([]);
+
+  const {} = useQuery(
+    ["posts"],
+    async () => (
+      await axiosClient.get("/posts/getAllPosts")).data,
+      {
+        onSuccess: (data) => {
+          setPostsList(data.response);
+          console.log('test');
+        },
+        refetchOnWindowFocus:false
+        // refetchInterval: 5000,
+      }
+  );
+
+  return (
     <>
-    <Box sx={{display:"flex", justifyContent:"end", mx:"2rem"}}>
-      <Button
+      <Box sx={{ display: "flex", justifyContent: "end", mx: "2rem" }}>
+        <Button
           align="center"
           variant="outlined"
           size="large"
-          sx={{ color: "black", borderColor: "black"}}
+          sx={{ color: "black", borderColor: "black" }}
         >
-         <AddIcon sx={{mr:"0.3rem"}}/> Add a Post 
+          <AddIcon sx={{ mr: "0.3rem" }} /> Add a Post
         </Button>
-    </Box>
-<Box className="cardBox" sx={{display:"flex", justifyContent:"center"}}>
-<Card className="cardDisplay">
-{/* <CardMedia
-  component="img"
-  height="140"
-  src="./images/test.jpg"
-  alt="green iguana"
-/> */}
-<CardContent>
-  <Typography gutterBottom variant="h5" component="div">
-    Something
-  </Typography>
-  {/* <Typography variant="body2" color="text.secondary">
-    Lizards are a widespread group of squamate reptiles, with over 6,000
-    species, ranging across all continents except Antarctica
-  </Typography> */}
-</CardContent>
-{/* <CardActions>
-  <Button size="small">Share</Button>
-  <Button size="small">Learn More</Button>
-</CardActions> */}
-</Card>
-</Box>
-</>
-  ) 
-};
+      </Box>
+      <CreatePostForm />
+      <PostsList postsList={postsList} />
+    </>
+  );
+}; 
 
 export default Feed;
